e6546fb52667986ac02dfc2450c9a51041332850
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-05-01 20:10:38 -0500

    update controller method added

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::resource('products', ProductController::class);
 
 Route::post('/products', [ProductController::class, 'store']);
 
