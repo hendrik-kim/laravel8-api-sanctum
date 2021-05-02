db659af4faabff8019db7df98e305b8cd0f55f94
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-05-01 20:34:40 -0500

    organize route for auth

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,20 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('products', ProductController::class);
 
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get(
-        '/products/search/{name}',
-        [ProductController::class, 'search']
-    );
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
