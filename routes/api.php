778be23aef50bcd1fa8450cc748704a03a5d77fe
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-05-01 19:49:23 -0500

    add model

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', function(){
-    return 'products';
+Route::get('/products', function () {
+    return Product::all();
+});
+
+Route::post('/products', function () {
+    return Product::create(['name' => 'Product One', 'slug' => 'product-slug', 'descr' => 'description', 'price' => 99.99]);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
